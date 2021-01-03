5d156e2fbe618eb7c3b3fcd50ae18479d2578b86
Daniele <tortora87@gmail.com>
2021-01-03 19:49:26 +0100

    remove window scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,13 @@
-import styled from "@emotion/styled";
 import About from "components/About/About";
 import Contact from "components/Contact/Contact";
 import Footer from "components/Footer/Footer";
 import Sidebar from "components/Sidebar/Sidebar";
 import Work from "components/Work/Work";
 import Head from "next/head";
-import { useEffect } from "react";
 import Hero from "../components/Hero/Hero";
 import Header from "components/Header/Header";
 
 export default function Home() {
-  useEffect(() => {
-    if (window.location.hash) {
-      const elToScroll = document.querySelector(window.location.hash);
-
-      console.log(elToScroll);
-
-      elToScroll.scrollIntoView();
-    }
-  }, []);
   return (
     <>
       <Head>

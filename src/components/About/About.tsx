d02a2fc90b967ad5620ef033c1005a7b71835720
Daniele <tortora87@gmail.com>
2021-01-03 12:52:15 +0100

    fix typ

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Heading2 from "shared/Heading2";
-import HorizonalLines from "shared/HorizonalLines";
 import { Section, Text, SubTitle, Flexbox } from "./About.styled";
 
 interface Props {}
@@ -12,7 +11,7 @@ const About = (props: Props) => {
         <Heading2>About Me</Heading2>
         <SubTitle>Ciao!</SubTitle>
         <Text>
-          I am a Frontend Engineer who is passionate about creating rich user
+          I am a Software Engineer who is passionate about creating rich user
           interfaces and interactive applications using JavaScript.
         </Text>
         <Text>
@@ -20,13 +19,15 @@ const About = (props: Props) => {
           coupled code that is easy to extend, maintain and reuse.
         </Text>
         <Text>
-          Along with exhaustive experience in using HTML, CSS, JavaScript,
-          Accessibility and Web Performances, the list of tools I'm proficient
-          in using includes React, React Native, Vue.js, TypeScript, Next.js,
-          Redux, RxJS, GraphQL, Apollo, Storybook, Gatsby, Node, Express,
-          Webpack, Jest, Cypress, Detox, Google Analytics, Mixpanel, Optimizely,
-          Datadog, CircleCi. I am also passionate about UX/UI Design, Usability
-          Testing and wireframe prototyping using Figma.
+          Along with exhaustive experience in using HTML, CSS, JavaScript, the
+          list of tools I'm proficient in using includes React, React Native,
+          Vue.js, TypeScript, Next.js, Redux, RxJS, GraphQL, Apollo, Storybook,
+          Gatsby, Node, Express, Webpack, Jest, Cypress, Detox, Google
+          Analytics, Mixpanel, Optimizely, Datadog, CircleCi.
+        </Text>
+        <Text>
+          I am also interested in UX/UI Design, Usability Testing and User
+          Research.
         </Text>
       </Flexbox>
     </Section>

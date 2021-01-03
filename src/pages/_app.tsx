d7ede977799a105ff99d96fe7ea27ac5b8db85de
Daniele <tortora87@gmail.com>
2021-01-03 18:02:30 +0100

    add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+// TODO need the no-check because of GA script, will need to come back to it;
 import React from "react";
 import { GlobalStyles, theme } from "../styles";
 import { ThemeProvider } from "@emotion/react";
@@ -5,7 +7,6 @@ import Header from "components/Header/Header";
 import Head from "next/head";
 import { SidebarProvider } from "hooks/context/SidebarContext";
 import { init } from "emailjs-com";
-
 function MyApp({ Component, pageProps }) {
   React.useEffect(() => {
     init(process.env.NEXT_USERID);
@@ -22,6 +23,16 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NRMJHR8YZQ"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config',
+          'G-NRMJHR8YZQ');
+        </script>
       </Head>
       <ThemeProvider theme={theme}>
         <SidebarProvider>

4c1492803aadee2111bdc7aafd5c915cf849a736
Daniele <tortora87@gmail.com>
2021-01-03 18:19:07 +0100

    add a document file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { GlobalStyles, theme } from "../styles";
 import { ThemeProvider } from "@emotion/react";
 import Header from "components/Header/Header";
-import Head from "next/head";
 import { SidebarProvider } from "hooks/context/SidebarContext";
 import { init } from "emailjs-com";
 function MyApp({ Component, pageProps }) {
@@ -13,27 +12,6 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@100&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        {/* Global site tag (gtag.js) - Google Analytics */}
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-NRMJHR8YZQ"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config',
-          'G-NRMJHR8YZQ');
-        </script> */}
-      </Head>
       <ThemeProvider theme={theme}>
         <SidebarProvider>
           <GlobalStyles />
---
+++ src/pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@100&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=${YOUR_TRACKING_ID}"
+          ></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-NRMJHR8YZQ');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}

cd4174b4ee03adeb1689c8ddf192afba80aa178d
Daniele <tortora87@gmail.com>
2021-01-16 11:43:46 +0100

    add config

---
+++ config/index.ts
@@ -0,0 +1,31 @@
+export default {
+  title: `Daniele Tortora | Software Engineer`,
+  siteUrl: "https://www.danieletortora.com",
+  description: `I am a Software Engineer who is passionate about creating rich user interfaces and interactive applications using JavaScript.
+  I believe in the importance of writing clean, modular and loosely coupled code that is easy to extend, maintain and reuse.
+  Along with exhaustive experience in using HTML, CSS, JavaScript, Accessibility and Web Performances, the list of tools I'm proficient in using includes React, React Native, Vue.js, TypeScript,  Next.js, Redux, RxJS, GraphQL, Apollo, Storybook, Gatsby, Node, Express, Webpack, Jest, Cypress, Detox, Google Analytics, Mixpanel, Optimizely, Datadog, Jenkins, CircleCi.
+  I am also passionate about UX/UI Design, Usability Testing and wireframe prototyping using Figma.`,
+  keywords:
+    "Daniele Tortora, Daniele, Tortora, floroz, floroz87, Software Engineer, Frontend Engineer, Front End Engineer, Front End Developer, developer, software developer, web developer, javascript, london, italy, react developer",
+  author: `Daniele Tortora`,
+  siteLanguage: "en_GB",
+  location: "London, UK",
+  email: "contact@danieletortora.com",
+  github: "https://github.com/floroz",
+  twitter: "@floroz87",
+  googleAnalyticsTrackingId: "G-NRMJHR8YZQ",
+  socialMedia: [
+    {
+      name: "Github",
+      url: "https://github.com/floroz87",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/danieletortora",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/floroz87",
+    },
+  ],
+};

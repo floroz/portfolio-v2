4f4c6b2395ce3162afd134ea9a42bd0b9c3f9fb4
Daniele <tortora87@gmail.com>
2021-01-16 11:44:03 +0100

    SEO and metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript, Html } from "next/document";
+import metadata from "../../config";
 
 export default class MyDocument extends Document {
   render() {
@@ -14,9 +15,20 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <meta name="description" content={metadata.description} />
+          <meta name="og:title" content={metadata.title} />
+          <meta name="og:description" content={metadata.description} />
+          <meta name="og:type" content="website" />
+          <meta name="og:url" content={metadata.siteUrl} />
+          <meta name="og:title" content={metadata.title} />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:creator" content={metadata.author} />
+          <meta name="twitter:title" content={metadata.title} />
+          <meta name="twitter:description" content={metadata.description} />
+
           <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=${YOUR_TRACKING_ID}"
+            src={`https://www.googletagmanager.com/gtag/js?id=${metadata.googleAnalyticsTrackingId}`}
           ></script>
           <script
             async
@@ -24,7 +36,7 @@ export default class MyDocument extends Document {
               __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-NRMJHR8YZQ');`,
+              gtag('config', '${metadata.googleAnalyticsTrackingId}');`,
             }}
           />
         </Head>

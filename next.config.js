f09ebc4dc12ea14278b1e29ad5acd744afeb8b2a
Daniele <tortora87@gmail.com>
2021-01-21 09:10:25 +0100

    add svg loading to webpack

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const nextEnv = require("next-env");
-const dotenvLoad = require("dotenv-load");
-
-dotenvLoad();
-
-const withNextEnv = nextEnv();
-
-module.exports = withNextEnv({
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    });
+    return config;
+  },
   env: {
     NEXT_EMAIL_JS: process.env.NEXT_EMAIL_JS,
     NEXT_EMAIL_TEMPLATE: process.env.NEXT_EMAIL_TEMPLATE,
     NEXT_EMAIL_USERID: process.env.NEXT_EMAIL_USERID,
   },
-});
+};
